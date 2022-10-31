515c16b18a708711b5b0f7b60090ce7cfd4ecfae
Sylvebois <thomasghaye@hotmail.com>
2022-10-31 14:51:16 +0100

    Added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Map from './Components/PlomboZone/Map';
+import PlomboImg from './Components/PlomboZone/PlomboImg';
+import PlomboSpeech from './Components/TextZone/PlomboSpeech';
+import PlomboText from './Components/TextZone/PlomboText';
+import Dictaphone from './Components/TextZone/Dictaphone';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { page: 'home' }
+
+  handleButtonClick = navTo => {
+    this.setState({ page: navTo });
+  }
+
+  render() {
+    const page = this.state.page;
+    let borderStyle = '20px solid rgb(' + Math.random()*255 + ',' + Math.random()*255 + ',' + Math.random()*255 + ')';
+
+    return (
+      <div className="App">
+        {page.startsWith('lookingFor')? <Map page={page} /> : <PlomboImg page={page} />}
+        <div className="textZone" style={{ border: borderStyle}}>
+          <PlomboSpeech page={page} />
+          <div className="choiceButton">
+            <PlomboText page={page} onClick={this.handleButtonClick} />
+          </div>
+        </div>
+        <Dictaphone page={page} onClick={this.handleButtonClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
