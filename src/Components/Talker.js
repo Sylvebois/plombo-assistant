28bff2279f5be9861de78b91d9fdeeb34f0b4a7d
Sylvebois <thomasghaye@hotmail.com>
2023-09-15 15:32:13 +0200

    Updated talker for the jokes

--- src/Components/Talker.js
+++ src/Components/Talker.js
@@ -1,39 +1,55 @@
+import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { TextToSpeech } from 'tts-react'
+import { useTts } from 'tts-react'
 
 const Talker = ({ currSpeech }) => {
-  let text
+  const startingValue = typeof (currSpeech) === 'object' ? currSpeech[0] + ' ' + currSpeech[1] : currSpeech
+  const [currTxt, setCurrTxt] = useState(startingValue)
+  const [revealClass, setReveal] = useState('toReveal')
+  const { ttsChildren, play } = useTts({
+    children: currTxt,
+    markTextAsSpoken: true,
+    autoPlay: true,
+  })
+
+  const handleClick = () => {
+    setCurrTxt(currSpeech[2])
+    setReveal('reveal')
+  }
+
+  useEffect(() => {
+    if(typeof (currSpeech) === 'object') { 
+      const introTxt = currSpeech[0] + ' ' + currSpeech[1]
+      const answer = currSpeech[2]
+
+      if(currTxt !== introTxt && currTxt !== answer) {
+        setCurrTxt(introTxt)
+      }
+    }
+    else if (currSpeech !== currTxt) {
+      setCurrTxt(currSpeech)
+    }
+  }, [currSpeech, currTxt])
 
   if (typeof (currSpeech) === 'object') {
-    text = <>
-      <p>{currSpeech[0]}</p>
-      {currSpeech[1] === "" ? null : <p>{currSpeech[1]}</p>}
-      {currSpeech[2] === "" ? null : <p>{currSpeech[2]}</p>}
-    </>
+    return (
+      <>
+        <p>
+          { currSpeech[0] }
+          <br />
+          { currSpeech[1] === '' ? null : currSpeech[1] }
+        </p>
+        {
+          currSpeech[2] === '' ?
+            null :
+            <p className={revealClass} onClick={handleClick}>{currSpeech[2]}</p>
+        }
+      </>
+    )
   }
   else {
-    text = <p>{currSpeech}</p>
+    return (<p>{currSpeech}</p>)
   }
-
-  return (
-    <>
-      <TextToSpeech
-        markTextAsSpoken
-        align="horizontal"
-        size="medium"
-        position="leftCenter"
-        lang="fr-BE"
-        autoPlay={true}
-      >
-        {text}
-      </TextToSpeech>
-      {
-        typeof (currSpeech) === 'object' ?
-          <p style={{ fontSize: "2vh" }}>Source: blablagues.net</p> :
-          null
-      }
-    </>
-  )
 }
 
 Talker.propTypes = {
