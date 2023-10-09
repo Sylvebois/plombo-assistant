99e7c4a74a3dd6bfb612c697070f1c275e4f28f5
Sylvebois <thomasghaye@hotmail.com>
2023-10-09 13:22:28 +0000

    pathName correction

---
+++ src/components/MapImg.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+
+const MapImg = ({ currPage }) => {
+  const images = {
+    lookingFor: { path: require('../assets/baseMap.jpg'), alt: 'Carte de base' },
+    lookingForSpareParts: { path: require('../assets/wayToSpareParts.gif'), alt: 'Chemin vers le comptoir' },
+    lookingForSanitary: { path: require('../assets/wayToSanit.gif'), alt: 'Chemin vers le sanitaire' },
+    lookingForKitchen: { path: require('../assets/wayToKitchen.gif'), alt: 'Chemin vers les cuisines' },
+    lookingForHeating: { path: require('../assets/wayToChauffage.gif'), alt: 'Chemin vers le chauffage' },
+  }
+
+  const style = {
+    maxHeight: '45vh',
+    maxWidth: '45vw',
+    display: 'inline-block',
+    verticalAlign: 'middle'
+  }
+
+  return (
+    <img
+      style={style}
+      src={images[currPage].path}
+      alt={images[currPage].alt}
+    />)
+}
+
+MapImg.propTypes = {
+  currPage: PropTypes.string.isRequired
+}
+
+export default MapImg
