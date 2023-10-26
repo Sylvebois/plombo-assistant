88f50ad10a770fcbb0679bf3224f51163acc07ff
Sylvebois <thomasghaye@hotmail.com>
2023-10-26 14:25:58 +0000

    Updated url

--- src/services/ai.js
+++ src/services/ai.js
@@ -1,5 +1,5 @@
 const askToAI = async (request) => {
-  const response = await fetch(process.env.REACT_APP_LLM_URL, {
+  const response = await fetch(`${process.env.REACT_APP_BASE_URL}/askToLLM`, {
     method: 'POST',
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ query: request })
